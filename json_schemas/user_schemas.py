88f8752dc4f11bd7358498361fe6b62bdeccd91b
vladislav.bubnov <v.bubnov@yclients.tech>
2025-01-23 18:41:18 +0200

    first commit

---
+++ json_schemas/user_schemas.py
@@ -0,0 +1,102 @@
+get_user = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "data": {
+      "type": "object",
+      "properties": {
+        "id": {
+          "type": "number"
+        },
+        "email": {
+          "type": "string"
+        },
+        "first_name": {
+          "type": "string"
+        },
+        "last_name": {
+          "type": "string"
+        },
+        "avatar": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "avatar"
+      ]
+    },
+    "support": {
+      "type": "object",
+      "properties": {
+        "url": {
+          "type": "string"
+        },
+        "text": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "url",
+        "text"
+      ]
+    }
+  },
+  "required": [
+    "data",
+    "support"
+  ]
+}
+
+post_user = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "id": {
+      "type": "string"
+    },
+    "createdAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "id",
+    "createdAt"
+  ]
+}
+
+put_user = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "title": "Generated schema for Root",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "updatedAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "updatedAt"
+  ]
+}
+
---
+++ tests/test_crud_user.py
@@ -0,0 +1,59 @@
+import os
+import random
+
+import pytest
+import requests
+from faker import Faker
+
+from jsonschema import validate
+
+from json_schemas.user_schemas import post_user, get_user, put_user
+
+faker = Faker("ru_RU")
+url = "https://reqres.in"
+endpoint = "/api/users/"
+user_id = "2"
+
+
+def test_post_user_positive():
+    response = requests.post(url + endpoint, data={"name": f"{faker.name()}", "job": f"{faker.job()}"})
+
+    assert response.status_code == 201
+    validate(response.json(), post_user)
+
+
+@pytest.mark.parametrize("name, job", [
+    (faker.name(), None),
+    (None, faker.job())
+])
+def test_post_user_negative(name, job):
+    response = requests.post(url + endpoint, data={"name": name, "job": job})
+
+    assert response.status_code == 400
+
+
+def test_get_user_positive():
+    response = requests.get(url + endpoint + user_id)
+
+    assert response.status_code == 200
+    validate(response.json(), get_user)
+
+
+def test_get_user_negative():
+    response = requests.get(url + endpoint + "88")
+
+    assert response.status_code == 404
+    validate(response.json(), get_user)
+
+
+def test_put_user_info():
+    response = requests.put(url + endpoint + user_id, data={"name": f"{faker.name()}", "job": f"{faker.job()}"})
+
+    assert response.status_code == 200
+    body = response.json()
+    validate(body, put_user)
+
+
+def test_delete_user_info():
+    response = requests.delete(url + endpoint + user_id)
+    assert response.status_code == 204
